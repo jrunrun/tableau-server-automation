bc3863c61d2ccd7daf2026b26642613400259525
Justin Craycraft <jpcraycraft@gmail.com>
2022-05-11 16:10:07 -0500

    -added info on setting up cron job

--- tableau-log-cleanup.sh
+++ tableau-log-cleanup.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 tsm maintenance cleanup
+
+# https://stackoverflow.com/questions/30806318/how-to-cron-job-setup-in-amazon-ec2
+# [ec2-user@ip-172-31-26-126 tableau-server-automation]$ crontab -l
+# 0 21 * * * /home/ec2-user/tableau-server-automation/tableau-log-cleanup.sh
